d8c3fd77e9a857c3f5e4f89f634ee73cef1a29a1
Abigail <abby.a.otieno@gmail.com>
2023-10-26 10:02:39 +0300

    Completed Lab

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears VALUES(1,'Mr. Chocolate',13,'M','chestnut','grumpy',1);
+
+INSERT INTO bears VALUES(2,'Rowdy',8,'M','chestnut','slow',1);
+
+INSERT INTO bears VALUES(3,'Tabitha',5,'M','chestnut','energetic',1);
+
+INSERT INTO bears VALUES(4,'Sergeant Brown',12,'F','dark brown','quiet',1);
+
+INSERT INTO bears VALUES(5,'Melissa',7,'M','black','naive',1);
+
+INSERT INTO bears VALUES(6,'Grinch',10,'F','black','active',1);
+
+INSERT INTO bears VALUES(7,'Wendy',4,'M','chestnut','high',1);
+
+INSERT INTO bears VALUES(8,NULL,4,'F','light brown','low',1);
+
